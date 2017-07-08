5195200f10e9b9859e04117361cb661da7b0928b
mtytk <antiklios@gmail.com>
2017-07-08 18:28:37 +0300

    First commit

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Created by umit on 08/07/2017.
+ */
+public class App {
+    private final static String JEDIS_SERVER = "127.0.0.1";
+    private ArrayList<String> messageContainer = new ArrayList<String>();
+    public static void main(String[] args) {
+        
+    }
+}
